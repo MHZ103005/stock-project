497a94fa6deda845e4b1171b5d3fd2617842b814
MHZ103005 <haotianzheng@gmail.com>
2025-06-24 00:13:55 -0400

    Added python api call

--- API.py
+++ API.py
@@ -1,7 +1,23 @@
 import yfinance as yf
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-tickers = ["AAPL", "MSFT", "GOOG"]
-data = yf.download(tickers, period="1d")["Close"].iloc[-1]
+def fetchPrices(inFile = "tickers.txt", outFile = "prices.txt"):
+    with open(inFile, "r") as f:
+        line = f.readline().strip()
+        tickers = line.split()
+        f.truncate(0)
+    
+    with open(outFile, "w") as out:
+        for ticker in tickers:
+            try:
+                stock = yf.Ticker(ticker)
+                price = stock.history(period = "1d")["Close"].iloc[-1]
+                out.write(f"{price}\n")
+            except:
+                out.write("Ticker does not exist!")
 
-for ticker in tickers:
-    print(ticker, data[ticker])
+
+fetchPrices()
+print("File updated")
+
